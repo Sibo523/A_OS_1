d90e6933be0b5a5e339bc1c33947b01c378afe97
Roi Sibony <121120413+Sibo523@users.noreply.github.com>
2024-05-17 15:26:33 +0300

    Create findPhone.c

---
+++ Question_6/findPhone.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void usage(const char *prog_name) {
+    fprintf(stderr, "Usage: %s \"Name\"\n", prog_name);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        usage(argv[0]);
+    }
+
+    int pipe1[2];
+    int pipe2[2];
+    int pipe3[2];
+    
+    if (pipe(pipe1) == -1) {
+        perror("pipe1");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fork() == 0) {
+        // First child process: `cat phonebook.txt`
+        close(pipe1[0]);
+        dup2(pipe1[1], STDOUT_FILENO);
+        close(pipe1[1]);
+        execlp("cat", "cat", "phonebook.txt", (char *)NULL);
+        perror("execlp cat");
+        exit(EXIT_FAILURE);
+    }
+
+    close(pipe1[1]);
+
+    if (pipe(pipe2) == -1) {
+        perror("pipe2");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fork() == 0) {
+        // Second child process: `grep "Name"`
+        close(pipe2[0]);
+        dup2(pipe1[0], STDIN_FILENO);
+        close(pipe1[0]);
+        dup2(pipe2[1], STDOUT_FILENO);
+        close(pipe2[1]);
+        execlp("grep", "grep", argv[1], (char *)NULL);
+        perror("execlp grep");
+        exit(EXIT_FAILURE);
+    }
+
+    close(pipe1[0]);
+    close(pipe2[1]);
+
+    if (pipe(pipe3) == -1) {
+        perror("pipe3");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fork() == 0) {
+        // Third child process: `sed 's/,/ /'`
+        close(pipe3[0]);
+        dup2(pipe2[0], STDIN_FILENO);
+        close(pipe2[0]);
+        dup2(pipe3[1], STDOUT_FILENO);
+        close(pipe3[1]);
+        execlp("sed", "sed", "s/,/ /", (char *)NULL);
+        perror("execlp sed");
+        exit(EXIT_FAILURE);
+    }
+
+    close(pipe2[0]);
+    close(pipe3[1]);
+
+    if (fork() == 0) {
+        // Fourth child process: `awk '{print $2}'`
+        dup2(pipe3[0], STDIN_FILENO);
+        close(pipe3[0]);
+        execlp("awk", "awk", "{print $2}", (char *)NULL);
+        perror("execlp awk");
+        exit(EXIT_FAILURE);
+    }
+
+    close(pipe3[0]);
+
+    // Wait for all child processes to finish
+    while (wait(NULL) > 0);
+
+    return 0;
+}
